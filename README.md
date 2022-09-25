ff1d0c19fe67c5d94bf322b167bdb86a14383e13
Víctor Enrique Aguirre <68919985+kikewebcity@users.noreply.github.com>
2022-09-24 20:56:07 -0500

    Initial commint

---
+++ README.md
@@ -0,0 +1,3 @@
+# TEST-ALO
+website  of glass artists 
+Un website para los artistas, artesanos y personas apasionadas por el vidrio, la cerámica y la joyería. 
---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 1, y: -45 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1, y: -45  });
+gsap.from(".titulo-portada h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
